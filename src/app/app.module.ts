123b6ff86c72897581c796ac1c0513297f7a24c9
ShrawanVD <shra1.deo7714@gmail.com>
2024-04-02 14:25:00 +0530

    overlat folder created - but its not implemented as swiper is not executing properly. Also some minute changes are done in the index page - need to do the overlay page afterwards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CandidateformComponent } from './components/candidateform/candidateform.component';
+// import { OverlayComponent } from './components/overlay/overlay.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    CandidateformComponent
+    CandidateformComponent,
+    // OverlayComponent
   ],
   imports: [
     BrowserModule,
