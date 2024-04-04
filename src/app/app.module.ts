b4503dfddfe1a17c8ebc05b027279fdd1a1fc4de
ShrawanVD <shra1.deo7714@gmail.com>
2024-04-04 11:46:31 +0530

    ui of company form and included many libraries in app.module.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,27 @@ import { OverlayComponent } from './components/overlay/overlay.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { CompanyComponent } from './components/company/company.component';
 
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
+import {MatChipsModule} from '@angular/material/chips';
+
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {Component, inject} from '@angular/core';
+import {MatChipEditedEvent, MatChipInputEvent} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {NgFor} from '@angular/common';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+
+import {MatSelectModule} from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
+
+
+import {MatDialogModule} from '@angular/material/dialog';
+import {NgIf} from '@angular/common';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +42,20 @@ import { CompanyComponent } from './components/company/company.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatChipsModule,
+    MatCardModule,
+    NgFor, 
+    MatIconModule,
+    MatSelectModule,
+    MatDialogModule,
+    NgIf,
   ],
   providers: [
     provideClientHydration(),
@@ -29,4 +63,6 @@ import { CompanyComponent } from './components/company/company.component';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+ }
