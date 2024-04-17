9617e618289d9f3038deadfacdd12878cc89458e
ShrawanVD <shra1.deo7714@gmail.com>
2024-04-17 18:33:31 +0530

    added meet the team but 1200px responsiveness is remaining

--- src/app/components/candidateform/candidateform.component.ts
+++ src/app/components/candidateform/candidateform.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CandidateService } from '../../services/candidate.service';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+import { MatStepper } from '@angular/material/stepper';
 
 
 
@@ -14,14 +15,23 @@ import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 })
 export class CandidateformComponent  implements OnInit{
 
+
+
   firstFormGroup!: FormGroup;
   secondFormGroup!: FormGroup;
   thirdFormGroup!: FormGroup;
   fourFormGroup!: FormGroup;
   fiveFormGroup!: FormGroup;
   formData: any;
+  firstNext:boolean = false;
+  secondNext:boolean = false;
+  thirdNext:boolean = false;
+  fourthNext:boolean = false;
 
-  constructor(private _formBuilder: FormBuilder,public candidate: CandidateService, private _snackBar: MatSnackBar,public router:Router) {}
+  @ViewChild(MatStepper) stepper!: MatStepper;
+
+  constructor(private _formBuilder: FormBuilder, public candidate: CandidateService, private _snackBar: MatSnackBar,public router:Router, ) {}
+
 
 
   ngOnInit(): void {
@@ -43,34 +53,31 @@ export class CandidateformComponent  implements OnInit{
       dialects: ['', Validators.required],
     });
     this.secondFormGroup = this._formBuilder.group({
-      supdatedLanguage: [''],
-        sWorkshop: [''],
-        sLangCommunity: [''],
-        sLiterature: [''],
-        sLangIntoWork: [''],
-        sMethodLang: [''],
-        sgramm: ['']
+      supdatedLanguage: ['', Validators.required],
+        sWorkshop: ['', Validators.required],
+        sLangCommunity: ['', Validators.required],
+        sLiterature: ['', Validators.required],
+        sLangIntoWork: ['', Validators.required],
+        sMethodLang: ['', Validators.required],
+        sgramm: ['', Validators.required]
     });
     this.thirdFormGroup = this._formBuilder.group({
-      tTools: [''],
-      totherTools: [''],
-        tTranslation: [''],
-        tdictionaries: [''],
-        tdevelopedTool: [''],
-        tgeneOutput: ['']
+      tTools: ['', Validators.required],
+      totherTools: ['', Validators.required],
+        tTranslation: ['', Validators.required],
+        tdictionaries: ['', Validators.required],
+        tdevelopedTool: ['', Validators.required],
+        tgeneOutput: ['', Validators.required]
     });
     this.fourFormGroup = this._formBuilder.group({
-      fproficient: [''],
-        fFormalTrainer: [''],
-        fworkExample: [''],
-        fiInterpretation: [''],
-        fichallenges: [''],
-        fiLanguageSkills: [''],
+      fproficient: ['', Validators.required],
+        fFormalTrainer: ['', Validators.required],
+        fworkExample: ['', Validators.required],
     });
     this.fiveFormGroup = this._formBuilder.group({
-        fiInterpretation: [''],
-        fichallenges: [''],
-        fiLanguageSkills: [''],
+        fiInterpretation: ['', Validators.required],
+        fichallenges: ['', Validators.required],
+        fiLanguageSkills: ['', Validators.required],
     });
   }
 
@@ -126,12 +133,63 @@ export class CandidateformComponent  implements OnInit{
       config.duration = 1000;
       config.verticalPosition = 'top'; 
       config.panelClass = ['custom-snackbar']; 
-      this._snackBar.open('Please Fill The Form', 'Close', config);
+      this._snackBar.open('Please fill the form', 'Close', config);
     }
 
   }
   
-
+  checkData1(){
+    if(this.firstFormGroup.valid){
+      this.firstNext = true;
+      this.stepper.next();
+      // matStepperNext
+    }else{
+      const config = new MatSnackBarConfig();
+      config.duration = 1000;
+      config.verticalPosition = 'top'; 
+      config.panelClass = ['custom-snackbar']; 
+      this._snackBar.open('Please fill the current section', 'Close', config);
+    }
+  }
+  checkData2(){
+    if(this.secondFormGroup.valid){
+      this.secondNext = true;
+      this.stepper.next();
+      // matStepperNext
+    }else{
+      const config = new MatSnackBarConfig();
+      config.duration = 1000;
+      config.verticalPosition = 'top'; 
+      config.panelClass = ['custom-snackbar']; 
+      this._snackBar.open('Please fill the current section', 'Close', config);
+    }
+  }
+  checkData3(){
+    if(this.thirdFormGroup.valid){
+      this.thirdNext = true;
+      this.stepper.next();
+      // matStepperNext
+    }else{
+      const config = new MatSnackBarConfig();
+      config.duration = 1000;
+      config.verticalPosition = 'top'; 
+      config.panelClass = ['custom-snackbar']; 
+      this._snackBar.open('Please fill the current section', 'Close', config);
+    }
+  }
+  checkData4(){
+    if(this.fourFormGroup.valid){
+      this.fourthNext = true;
+      this.stepper.next();
+      // matStepperNext
+    }else{
+      const config = new MatSnackBarConfig();
+      config.duration = 1000;
+      config.verticalPosition = 'top'; 
+      config.panelClass = ['custom-snackbar']; 
+      this._snackBar.open('Please fill the current section', 'Close', config);
+    }
+  }
   
 
 
