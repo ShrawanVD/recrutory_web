f50567bd8cc28fc029a6fe4ace8f6d1a5d3db7f1
vaibhavkwagh <vaibhavkw2001@gmail.com>
2024-05-16 11:02:20 +0530

    fixed the blogs scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recrutoryweb';
+  constructor(private router: Router, private viewportScroller: ViewportScroller) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.viewportScroller.scrollToPosition([0, 0]);
+      }
+    });
+  }
 }
